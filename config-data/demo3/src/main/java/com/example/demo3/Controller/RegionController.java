466ae7e77b05b8a7387969adddfdea776247db3a
aferreira <aferreira@cabal.com.py>
2023-06-29 08:23:52 -0400

    Re fix application.yml

--- config-data/demo3/src/main/java/com/example/demo3/Controller/RegionController.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo3.Controller;
-
-public class RegionController {
-    
-}
--- config-data/demo4/src/main/java/com/example/demo3/Repository/InvoiceItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo3.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo3.Entity.InvoiceItem;
-
-public interface InvoiceItemRepository extends JpaRepository<InvoiceItem,Long> {
-    
-}
